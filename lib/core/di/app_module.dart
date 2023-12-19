bf9538e063b6c9ac3fb4f905acc520b1b6573c57
Martin Juhel <juhel.martin@gmail.com>
2023-12-19 20:11:49 +0100

    add firebaseCrashlytics

--- lib/core/di/app_module.dart
+++ lib/core/di/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:bpm/core/data/services/firebase_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -12,6 +13,9 @@ abstract class AppModule {
 
   @injectable
   FirebaseFirestore get store => FirebaseFirestore.instance;
+
+  @injectable
+  FirebaseCrashlytics get crashlytics => FirebaseCrashlytics.instance;
 
   @injectable
   FirebaseAuth get auth => FirebaseAuth.instance;
