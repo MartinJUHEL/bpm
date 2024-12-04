d9414ebe5af756a2f75a230ac6dc70c44d37283d
martin.juhel <martin.juhel@niji.fr>
2024-12-04 17:03:39 +0100

    fix: tu for pipeline

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,13 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_test/flutter_test.dart';
 
-
-void main() {}
+void main() {
+  test(
+    'init test',
+    () {
+      expect(1, 1);
+    },
+  );
+}
